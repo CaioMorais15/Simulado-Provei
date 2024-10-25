64bafdefd715b918725cbec212d929cf403a4d3e
Fe-Corazza <felipecorazza09@gmail.com>
2024-10-25 14:23:34 -0300

    login e cadastro finalizados

--- Simulado/src/App.jsx
+++ Simulado/src/App.jsx
@@ -1,20 +1,24 @@
+import "./App.css";
 
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import Login from "./components/Login/Login";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/js/bootstrap.bundle.JS'
-import Navbar from './components/Navbar/Navbar'
-import Login from './components/Login/Login'
-
+import Cadastro from "./components/Cadastro/Cadastro";
+import Incluso from "./components/Incluso/Incluso";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Login />
-    </>
-
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/Cadastro" element={<Cadastro />} />
+        <Route path="/Incluso" element={<Incluso />} />
+        {/* Outras rotas */}
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ Simulado/src/components/Incluso/Incluso.jsx
@@ -0,0 +1,23 @@
+import "./Incluso.module.css";
+
+const Incluso = () => {
+  return (
+    <>
+      <div className="container">
+        <h1>O QUE ESTÁ INCLUSO?</h1>
+        <h2>Tudo Incluso? Entenda o Que Isso Significa!</h2>
+        <p className="description">
+          Você escolhe o veículo, o plano mensal e a franquia de quilometragem
+          que mais se adequa a você, e nós entregamos um carro novinho em folha,
+          com tudo incluso: documentação, IPVA, licenciamento, emplacamento,
+          seguro e manutenções. Tudo isso por uma mensalidade fixa. Sem
+          preocupações, sem estresse. No final do contrato, basta devolver o
+          veículo.
+        </p>
+      </div>
+      ;
+    </>
+  );
+};
+
+export default Incluso;
